a43e59ad68fb652cc40af35f90ad03763f7ede00
theNiceGirl <56582067+theNiceGirl-137@users.noreply.github.com>
2021-04-15 12:06:31 +0800

    19 --> 链表 && 双指针

---
+++ leetcode/editor/cn/[19]remove_nth_node_from_end_of_list.go
@@ -0,0 +1,70 @@
+//给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
+//
+// 进阶：你能尝试使用一趟扫描实现吗？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,2,3,4,5], n = 2
+//输出：[1,2,3,5]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1], n = 1
+//输出：[]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：head = [1,2], n = 1
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中结点的数目为 sz 
+// 1 <= sz <= 30 
+// 0 <= Node.val <= 100 
+// 1 <= n <= sz 
+// 
+// Related Topics 链表 双指针 
+// 👍 1316 👎 0
+
+
+package leetcode
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	var pre *ListNode
+	newHead := &ListNode{Next: head}
+	cur := newHead
+	if head == nil {
+		return head
+	}
+	count := 1
+	for head != nil {
+		if count >= n {
+			pre = cur
+			cur = cur.Next
+		}
+		count++
+		head = head.Next
+	}
+	pre.Next = pre.Next.Next
+	return newHead.Next
+}
+//leetcode submit region end(Prohibit modification and deletion)
