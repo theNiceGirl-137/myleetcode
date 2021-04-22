084e16b0f5d7fab0ac51c8c8663ef6171e84ee76
theNiceGirl <56582067+theNiceGirl-137@users.noreply.github.com>
2021-04-22 11:29:13 +0800

    633 --> 双指针

---
+++ leetcode/editor/cn/[633]sum_of_square_numbers.go
@@ -0,0 +1,66 @@
+//给定一个非负整数 c ，你要判断是否存在两个整数 a 和 b，使得 a2 + b2 = c 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：c = 5
+//输出：true
+//解释：1 * 1 + 2 * 2 = 5
+// 
+//
+// 示例 2： 
+//
+// 输入：c = 3
+//输出：false
+// 
+//
+// 示例 3： 
+//
+// 输入：c = 4
+//输出：true
+// 
+//
+// 示例 4： 
+//
+// 输入：c = 2
+//输出：true
+// 
+//
+// 示例 5： 
+//
+// 输入：c = 1
+//输出：true 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= c <= 231 - 1 
+// 
+// Related Topics 数学 
+// 👍 171 👎 0
+
+
+package leetcode
+
+import "math"
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func judgeSquareSum(c int) bool {
+	loop := math.Sqrt(float64(c))
+	l, r := 0, int(loop)
+	for l <= r {
+		res := l*l + r*r
+		if res > c {
+			r--
+		} else if res < c {
+			l++
+		} else {
+			return true
+		}
+	}
+	return false
+}
+//leetcode submit region end(Prohibit modification and deletion)
