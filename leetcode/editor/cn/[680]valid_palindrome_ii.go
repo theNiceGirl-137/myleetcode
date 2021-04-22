c2b1dde82a2cf03a33c85675e7f97ed08131b617
theNiceGirl <56582067+theNiceGirl-137@users.noreply.github.com>
2021-04-22 14:33:56 +0800

    680 --> 双指针

---
+++ leetcode/editor/cn/[680]valid_palindrome_ii.go
@@ -0,0 +1,54 @@
+//给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。 
+//
+// 示例 1: 
+//
+// 
+//输入: "aba"
+//输出: True
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: "abca"
+//输出: True
+//解释: 你可以删除c字符。
+// 
+//
+// 注意: 
+//
+// 
+// 字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。 
+// 
+// Related Topics 字符串 
+// 👍 342 👎 0
+
+
+package leetcode
+//leetcode submit region begin(Prohibit modification and deletion)
+func validPalindrome(s string) bool {
+	l, r := 0, len(s)-1
+	for l < r {
+		if s[l] != s[r] {
+			// 右边的字符向右移动一位，左边的字符向左移动一位，相当于移除左边或者右边的一个字符
+			// 区间左闭右开
+			return isPalindrome(s[l:r]) || isPalindrome(s[l+1:r+1])
+		}
+		l++
+		r--
+	}
+	return true
+}
+
+func isPalindrome(s string) bool {
+	l, r := 0, len(s)-1
+	for l < r {
+		if s[l] != s[r] {
+			return false
+		}
+		l++
+		r--
+	}
+	return true
+}
+//leetcode submit region end(Prohibit modification and deletion)
