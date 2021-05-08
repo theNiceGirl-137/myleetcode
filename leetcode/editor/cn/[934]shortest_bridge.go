063a608094c9ce7c889da01355719bd4660235ec
vickygao_i <vickygao_i@didiglobal.com>
2021-05-08 19:15:10 +0800

    934 --> unfinished

--- leetcode/editor/cn/[934]shortest_bridge.go
+++ leetcode/editor/cn/[934]shortest_bridge.go
@@ -40,64 +40,57 @@
 
 package leetcode
 //leetcode submit region begin(Prohibit modification and deletion)
-var direction934 = []int{-1, 0, 1, 0, 1}
+var direction934 = []int{-1, 0, 1, 0, -1}
 
+// https://leetcode-cn.com/problems/shortest-bridge/solution/go-by-louv-0bbj/
 func shortestBridge(A [][]int) int {
-	m, n := len(A), len(A[0])
-	points := make([][]int, 0)
+	queue := make([][2]int, 0)
 	flipped := false
-	for i :=0; i < m; i++ {
-		if flipped {
-			break
-		}
-		for j := 0; j < n; j++ {
+	for i := 0; i < len(A); i++ {
+		for j := 0; j < len(A[0]); j++ {
 			if A[i][j] == 1 {
-				dfs934(&points, &A, m, n, i, j)
+				dfs934(A, &queue, i, j)
 				flipped = true
 				break
 			}
 		}
+		if flipped {
+			break
+		}
 	}
-	var x, y, level int
-	for len(points) != 0 {
+	level := 0
+	for len(queue) > 0 {
 		level++
-		n_points := len(points)
-		for n_points != 0 {
-			n_points--
-			temp := points[n_points]
-			points = points[:n_points]
-			for k := 0; k < 4; k++ {
-				x = temp[0]+direction934[k]
-				y = temp[1]+direction934[k]
-				if x >= 0 && y >= 0 && x < m && y < n {
-					if A[x][y] == 2 {
-						continue
-					}
+		for i := 0; i < len(queue); i++ {
+			temp := queue[0]
+			queue = queue[1:]
+			for j := 0; j < 4; j++ {
+				x, y := temp[0]+direction934[j], temp[1]+direction934[j+1]
+				if x >= 0 && y >= 0 && x < len(A) && y < len(A[0]) && A[x][y] >= 0 {
 					if A[x][y] == 1 {
 						return level
 					}
-					points = append(points, []int{x, y})
-					A[x][y] = 2
+					A[x][y] = -1
+					queue = append(queue, [2]int{x, y})
 				}
 			}
 		}
 	}
-	return 0
+	return level
 }
 
-func dfs934(points, A *[][]int, m, n, i, j int) {
-	A1 := *A
-	if i < 0 || j < 0 || i == m || j == n || A1[i][j] == 2 {
-		return
+func dfs934(A [][]int, queue *[][2]int, i, j int) {
+	A[i][j] = -1
+	for k := 0; k < 4 ; k++ {
+		x, y := i+direction934[k], j+direction934[k+1]
+		if x >= 0 && y >= 0 && x < len(A) && y < len(A[0]) {
+			if A[x][y] == 0 {
+				*queue = append(*queue, [2]int{x, y})
+			}
+			if A[x][y] == 1 {
+				dfs934(A, queue, x, y)
+			}
+		}
 	}
-	if A1[i][j] == 0 {
-		*points = append(*points, []int{i, j})
-		return
-	}
-	A1[i][j] = 2
-	dfs934(points, A, m, n, i-1, j)
-	dfs934(points, A, m, n, i+1, j)
-	dfs934(points, A, m, n, i, j-1)
-	dfs934(points, A, m, n, i, j+1)
 }
 //leetcode submit region end(Prohibit modification and deletion)
