c25d3da2616e38720b94e877118505949d8fe3d2
vickygao_i <vickygao_i@didiglobal.com>
2021-05-10 13:53:22 +0800

    934 --> bfs

--- leetcode/editor/cn/[934]shortest_bridge.go
+++ leetcode/editor/cn/[934]shortest_bridge.go
@@ -43,54 +43,62 @@ package leetcode
 var direction934 = []int{-1, 0, 1, 0, -1}
 
 func shortestBridge(A [][]int) int {
-	queue := make([][2]int, 0)
+	m, n := len(A), len(A[0])
+	points := make([][2]int, 0)
 	flipped := false
-	for i := 0; i < len(A); i++ {
-		for j := 0; j < len(A[0]); j++ {
+	// dfs 寻找第一个岛屿，并把 1 全部赋值为 2
+	for i := 0; i < m; i++ {
+		if flipped {
+			break
+		}
+		for j := 0; j < n; j++ {
 			if A[i][j] == 1 {
-				dfs934(A, &queue, i, j)
+				dfs934(&points, A, m, n, i, j)
 				flipped = true
 				break
 			}
 		}
-		if flipped {
-			break
-		}
 	}
-	level := 0
-	for len(queue) > 0 {
+	// bfs 寻找第二个岛屿，并把过程中经过的 0 赋值为 2
+	var x, y, level int
+	for len(points) > 0 {
 		level++
-		length := len(queue)
-		for i := 0; i < length; i++ {
-			temp := queue[0]
-			queue = queue[1:]
-			for j := 0; j < 4; j++ {
-				x, y := temp[0]+direction934[j], temp[1]+direction934[j+1]
-				if x >= 0 && y >= 0 && x < len(A) && y < len(A[0]) && A[x][y] >= 0 {
-					if A[x][y] == 1 {
+		nPoints := len(points)
+		for nPoints > 0 {
+			temp := points[0]
+			points = points[1:]
+			nPoints--
+			for k := 0; k < 4; k++ {
+				x = temp[0]+direction934[k]
+				y = temp[1]+direction934[k+1]
+				if x >= 0 && y >= 0 && x <m && y < n {
+					if A[x][y] == 2 {
+						continue
+					}
+					if A[x][y] ==  1 {
 						return level
 					}
-					A[x][y] = -1
-					queue = append(queue, [2]int{x, y})
+					points = append(points, [2]int{x, y})
+					A[x][y] = 2
 				}
 			}
 		}
 	}
-	return level
+	return 0
 }
 
-func dfs934(A [][]int, queue *[][2]int, i, j int) {
-	A[i][j] = -1
-	for k := 0; k < 4 ; k++ {
-		x, y := i+direction934[k], j+direction934[k+1]
-		if x >= 0 && y >= 0 && x < len(A) && y < len(A[0]) {
-			if A[x][y] == 0 {
-				*queue = append(*queue, [2]int{x, y})
-			}
-			if A[x][y] == 1 {
-				dfs934(A, queue, x, y)
-			}
-		}
+func dfs934(points *[][2]int, A [][]int, m, n, i, j int) {
+	if i < 0 || j < 0 || i == m || j == n || A[i][j] == 2 {
+		return
 	}
+	if A[i][j] == 0 {
+		*points = append(*points, [2]int{i, j})
+		return
+	}
+	A[i][j] = 2
+	dfs934(points, A, m, n, i-1, j)
+	dfs934(points, A, m, n, i+1, j)
+	dfs934(points, A, m, n, i, j-1)
+	dfs934(points, A, m, n, i, j+1)
 }
 //leetcode submit region end(Prohibit modification and deletion)
