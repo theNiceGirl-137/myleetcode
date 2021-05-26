5bce417eccdd55ae2f0926fe2dd096f3ca7b9107
vickygao_i <vickygao_i@didiglobal.com>
2021-05-26 19:06:50 +0800

    310 --> bfs

--- leetcode/editor/cn/[310]minimum_height_trees.go
+++ leetcode/editor/cn/[310]minimum_height_trees.go
@@ -59,77 +59,47 @@
 
 
 package leetcode
-
-import "sort"
-
 //leetcode submit region begin(Prohibit modification and deletion)
-var direction310 = []int{-1, 0, 1, 0, -1}
-
 func findMinHeightTrees(n int, edges [][]int) []int {
-	sort.Slice(edges, func(i, j int) bool {
-		return edges[i][0] < edges[j][0]
-	})
-	return nil
+	var res []int
+	if n == 1 {
+		return append(res, 0)
+	}
+	// 统计节点出度
+	degree := make([]int, n)
+	// 统计节点之间的关系
+	edgeMap := make([][]int, n)
+	for _, edge := range edges {
+		degree[edge[0]]++
+		degree[edge[1]]++
+		edgeMap[edge[0]] = append(edgeMap[edge[0]], edge[1])
+		edgeMap[edge[1]] = append(edgeMap[edge[1]], edge[0])
+	}
+	queue := make([]int, 0)
+	// 把所有出度为 1 的节点入队，即叶子节点
+	for i := 0; i < n; i++ {
+		if degree[i] == 1 {
+			queue = append(queue, i)
+		}
+	}
+	for len(queue) > 0 {
+		// 每次循环都新建一个结果集，保证最后一定是最小高度树的节点集合
+		res = make([]int, 0)
+		// 记录每一层的节点数
+		length := len(queue)
+		for i := 0; i < length; i++ {
+			cur := queue[0]
+			queue = queue[1:]
+			res = append(res, cur)
+			neighbors := edgeMap[cur]
+			for _, neighbor := range neighbors {
+				degree[neighbor]--
+				if degree[neighbor] == 1 {
+					queue = append(queue, neighbor)
+				}
+			}
+		}
+	}
+	return res
 }
 //leetcode submit region end(Prohibit modification and deletion)
-//import java.util.ArrayList;
-//import java.util.LinkedList;
-//import java.util.List;
-//import java.util.Queue;
-//
-//public class FindMinHeightTrees {
-//public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-//List<Integer> res = new ArrayList<>();
-///*如果只有一个节点，那么他就是最小高度树*/
-//if (n == 1) {
-//res.add(0);
-//return res;
-//}
-///*建立各个节点的出度表*/
-//int[] degree = new int[n];
-///*建立图关系，在每个节点的list中存储相连节点*/
-//List<List<Integer>> map = new ArrayList<>();
-//for (int i = 0; i < n; i++) {
-//map.add(new ArrayList<>());
-//}
-//for (int[] edge : edges) {
-//degree[edge[0]]++;
-//degree[edge[1]]++;/*出度++*/
-//map.get(edge[0]).add(edge[1]);/*添加相邻节点*/
-//map.get(edge[1]).add(edge[0]);
-//}
-///*建立队列*/
-//Queue<Integer> queue = new LinkedList<>();
-///*把所有出度为1的节点，也就是叶子节点入队*/
-//for (int i = 0; i < n; i++) {
-//if (degree[i] == 1) queue.offer(i);
-//}
-///*循环条件当然是经典的不空判断*/
-//while (!queue.isEmpty()) {
-//res = new ArrayList<>();/*这个地方注意，我们每层循环都要new一个新的结果集合，
-//  这样最后保存的就是最终的最小高度树了*/
-//int size = queue.size();/*这是每一层的节点的数量*/
-//for (int i = 0; i < size; i++) {
-//int cur = queue.poll();
-//res.add(cur);/*把当前节点加入结果集，不要有疑问，为什么当前只是叶子节点为什么要加入结果集呢?
-//  因为我们每次循环都会新建一个list，所以最后保存的就是最后一个状态下的叶子节点，
-//  这也是很多题解里面所说的剪掉叶子节点的部分，你可以想象一下图，每层遍历完，
-//  都会把该层（也就是叶子节点层）这一层从队列中移除掉，
-//  不就相当于把原来的图给剪掉一圈叶子节点，形成一个缩小的新的图吗*/
-//List<Integer> neighbors = map.get(cur);
-///*这里就是经典的bfs了，把当前节点的相邻接点都拿出来，
-// * 把它们的出度都减1，因为当前节点已经不存在了，所以，
-// * 它的相邻节点们就有可能变成叶子节点*/
-//for (int neighbor : neighbors) {
-//degree[neighbor]--;
-//if (degree[neighbor] == 1) {
-///*如果是叶子节点我们就入队*/
-//queue.offer(neighbor);
-//}
-//}
-//}
-//}
-//return res;/*返回最后一次保存的list*/
-//}
-//
-//}
