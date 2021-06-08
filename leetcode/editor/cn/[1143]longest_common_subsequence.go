c7ca40ce24557c63fc1a17f9a1e1e90f1eb735fb
vickygao_i <vickygao_i@didiglobal.com>
2021-06-08 19:02:30 +0800

    1143 --> 动态规划

---
+++ leetcode/editor/cn/[1143]longest_common_subsequence.go
@@ -0,0 +1,78 @@
+//给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。 
+//
+// 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。 
+//
+// 
+// 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。 
+// 
+//
+// 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：text1 = "abcde", text2 = "ace" 
+//输出：3  
+//解释：最长公共子序列是 "ace" ，它的长度为 3 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：text1 = "abc", text2 = "abc"
+//输出：3
+//解释：最长公共子序列是 "abc" ，它的长度为 3 。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：text1 = "abc", text2 = "def"
+//输出：0
+//解释：两个字符串没有公共子序列，返回 0 。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= text1.length, text2.length <= 1000 
+// text1 和 text2 仅由小写英文字符组成。 
+// 
+// Related Topics 动态规划 
+// 👍 576 👎 0
+
+
+package leetcode
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func longestCommonSubsequence(text1 string, text2 string) int {
+	// 定义一个 dp 数组，其中 dp[i] 表示到位置 i 为止的子序列的性质，并不必须以 i 结尾
+	// 本题定义一个二维 dp 数组，其中 dp[i][j] 表示到第一个字符串位置 i 为止、到第二个字符串位置 j 为止，最长的公共子序列长度
+	m, n := len(text1), len(text2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	for i := 1; i <= m; i++ {
+		for j := 1; j <= n; j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1]+1
+			} else {
+				dp[i][j] = max1143(dp[i-1][j], dp[i][j-1])
+			}
+		}
+	}
+	return dp[m][n]
+}
+
+func max1143(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+//leetcode submit region end(Prohibit modification and deletion)
