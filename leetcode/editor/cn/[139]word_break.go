94b698016dbb35f2d3e18b2f07b36e8a48c89c4e
vickygao_i <vickygao_i@didiglobal.com>
2021-06-08 11:10:46 +0800

    139 --> 动态规划

---
+++ leetcode/editor/cn/[139]word_break.go
@@ -0,0 +1,48 @@
+//给定一个非空字符串 s 和一个包含非空单词的列表 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。 
+//
+// 说明： 
+//
+// 
+// 拆分时可以重复使用字典中的单词。 
+// 你可以假设字典中没有重复的单词。 
+// 
+//
+// 示例 1： 
+//
+// 输入: s = "leetcode", wordDict = ["leet", "code"]
+//输出: true
+//解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+// 
+//
+// 示例 2： 
+//
+// 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+//输出: true
+//解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+//     注意你可以重复使用字典中的单词。
+// 
+//
+// 示例 3： 
+//
+// 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+//输出: false
+// 
+// Related Topics 动态规划 
+// 👍 1013 👎 0
+
+
+package leetcode
+//leetcode submit region begin(Prohibit modification and deletion)
+func wordBreak(s string, wordDict []string) bool {
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	for i := 1; i <= len(s); i++ {
+		for _, word := range wordDict {
+			if i >= len(word) && s[i-len(word):i] == word {
+				dp[i] = dp[i] || dp[i-len(word)]
+			}
+		}
+	}
+	return dp[len(s)]
+}
+//leetcode submit region end(Prohibit modification and deletion)
