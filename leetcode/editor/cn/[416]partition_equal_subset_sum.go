11a10906f95eaef7bf0c6766d870f0b467dc7e37
vickygao_i <vickygao_i@didiglobal.com>
2021-06-17 19:29:31 +0800

    416 --> 动态规划

---
+++ leetcode/editor/cn/[416]partition_equal_subset_sum.go
@@ -0,0 +1,57 @@
+//给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,5,11,5]
+//输出：true
+//解释：数组可以分割成 [1, 5, 5] 和 [11] 。 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1,2,3,5]
+//输出：false
+//解释：数组不能分割成两个元素和相等的子集。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 200 
+// 1 <= nums[i] <= 100 
+// 
+// Related Topics 动态规划 
+// 👍 832 👎 0
+
+
+package leetcode
+//leetcode submit region begin(Prohibit modification and deletion)
+func canPartition(nums []int) bool {
+	// 0-1 背包问题
+	var sum, target, n int
+	for _, v := range nums {
+		sum += v
+	}
+	if sum % 2 == 1 {
+		return false
+	}
+	target, n = sum/2, len(nums)
+	// dp[i][j] 表示前 i 个数，是否能够和为 j
+	dp := make([][]bool, n+1)
+	for i := range dp {
+		dp[i] = make([]bool, target+1)
+		dp[i][0] = true
+	}
+	for i := 1; i <= n; i++ {
+		for j := nums[i-1]; j <= target; j++ {
+			dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]]
+		}
+	}
+	return dp[n][target]
+}
+//leetcode submit region end(Prohibit modification and deletion)
