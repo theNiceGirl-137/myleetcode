6f859aa6e9e53bc57ce8754ad8e7c1e6921902a0
theNiceGirl-137 <sqllQAQ@gmail.com>
2021-07-15 23:28:26 +0800

    53 --> 动态规划

---
+++ leetcode/editor/cn/[53]maximum_subarray.go
@@ -0,0 +1,84 @@
+//给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+//输出：6
+//解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1]
+//输出：1
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [0]
+//输出：0
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：nums = [-1]
+//输出：-1
+// 
+//
+// 示例 5： 
+//
+// 
+//输入：nums = [-100000]
+//输出：-100000
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 3 * 104 
+// -105 <= nums[i] <= 105 
+// 
+//
+// 
+//
+// 进阶：如果你已经实现复杂度为 O(n) 的解法，尝试使用更为精妙的 分治法 求解。 
+// Related Topics 数组 分治 动态规划 
+// 👍 3418 👎 0
+
+
+package leetcode
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func maxSubArray(nums []int) int {
+	if len(nums) < 1 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	// dp 数组表示下标到 i 为止的最大子序列的和
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	result := dp[0]
+	for i := 1; i < len(nums); i++ {
+		dp[i] = max53(dp[i-1]+nums[i], nums[i])
+		result = max53(dp[i], result)
+	}
+	return result
+}
+
+func max53(i, j int) int {
+	if i > j {
+		return i
+	}
+	return j
+}
+//leetcode submit region end(Prohibit modification and deletion)
