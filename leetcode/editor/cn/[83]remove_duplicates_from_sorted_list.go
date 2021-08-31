8faab0b5c9cf39050b8c77a012162977aa6e3324
theNiceGirl-137 <sqllQAQ@gmail.com>
2021-09-01 02:27:01 +0800

    83 --> 链表

---
+++ leetcode/editor/cn/[83]remove_duplicates_from_sorted_list.go
@@ -0,0 +1,55 @@
+//存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。 
+//
+// 返回同样按升序排列的结果链表。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：head = [1,1,2]
+//输出：[1,2]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：head = [1,1,2,3,3]
+//输出：[1,2,3]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表中节点数目在范围 [0, 300] 内 
+// -100 <= Node.val <= 100 
+// 题目数据保证链表已经按升序排列 
+// 
+// Related Topics 链表 👍 633 👎 0
+
+
+package leetcode
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteDuplicates(head *ListNode) *ListNode {
+	var next, temp *ListNode
+	temp = head
+	for head != nil && head.Next != nil {
+		next = head.Next
+		for  next != nil && head.Val == next.Val {
+			next = next.Next
+		}
+		head.Next = next
+		head = head.Next
+	}
+	return temp
+}
+//leetcode submit region end(Prohibit modification and deletion)
