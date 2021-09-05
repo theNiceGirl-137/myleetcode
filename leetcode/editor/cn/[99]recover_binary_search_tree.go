f11d4c8031b776a0b8a296e611cc71bae1ebfd73
theNiceGirl-137 <sqllQAQ@gmail.com>
2021-09-06 01:15:37 +0800

    99 --> 树

---
+++ leetcode/editor/cn/[99]recover_binary_search_tree.go
@@ -0,0 +1,68 @@
+//给你二叉搜索树的根节点 root ，该树中的两个节点被错误地交换。请在不改变其结构的情况下，恢复这棵树。 
+//
+// 进阶：使用 O(n) 空间复杂度的解法很容易实现。你能想出一个只使用常数空间的解决方案吗？ 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：root = [1,3,null,null,2]
+//输出：[3,1,null,null,2]
+//解释：3 不能是 1 左孩子，因为 3 > 1 。交换 1 和 3 使二叉搜索树有效。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [3,1,4,null,null,2]
+//输出：[2,1,4,null,null,3]
+//解释：2 不能在 3 的右子树中，因为 2 < 3 。交换 2 和 3 使二叉搜索树有效。 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树上节点的数目在范围 [2, 1000] 内 
+// -2³¹ <= Node.val <= 2³¹ - 1 
+// 
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 547 👎 0
+
+
+package leetcode
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+var x, y, parent *TreeNode
+
+func recoverTree(root *TreeNode) {
+	x, y, parent = nil, nil, nil
+	findXY(root)
+	x.Val, y.Val = y.Val, x.Val
+}
+
+func findXY(root *TreeNode) {
+	if root == nil {
+		return
+	}
+	// 中序遍历结果应该是顺序序列
+	findXY(root.Left)
+	if parent != nil && parent.Val > root.Val {
+		y = root
+		if x == nil {
+			x = parent
+		} else {
+			return
+		}
+	}
+	parent = root
+	findXY(root.Right)
+}
+//leetcode submit region end(Prohibit modification and deletion)
