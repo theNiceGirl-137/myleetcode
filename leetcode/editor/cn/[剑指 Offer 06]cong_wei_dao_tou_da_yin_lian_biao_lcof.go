2e78eba6c4e82b1a0ed8d940718025baa7de5000
theNiceGirl-137 <sqllQAQ@gmail.com>
2021-10-14 16:29:24 +0800

    剑指 Offer 06 --> 链表

---
+++ leetcode/editor/cn/[剑指 Offer 06]cong_wei_dao_tou_da_yin_lian_biao_lcof.go
@@ -0,0 +1,36 @@
+//输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：head = [1,3,2]
+//输出：[2,3,1] 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 链表长度 <= 10000 
+// Related Topics 栈 递归 链表 双指针 👍 194 👎 0
+
+
+package leetcode
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reversePrint(head *ListNode) []int {
+	ans := make([]int, 0)
+	for head != nil {
+		ans = append(ans, head.Val)
+		head = head.Next
+	}
+	reverse(ans)
+	return ans
+}
+//leetcode submit region end(Prohibit modification and deletion)
