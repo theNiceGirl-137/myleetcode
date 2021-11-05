2d84b48bb9a15ba975bb5e2b132da1be1add171d
theNiceGirl-137 <sqllQAQ@gmail.com>
2021-11-05 12:39:39 +0800

    1218 --> 动态规划

---
+++ leetcode/editor/cn/[1218]longest_arithmetic_subsequence_of_given_difference.go
@@ -0,0 +1,55 @@
+//给你一个整数数组 arr 和一个整数 difference，请你找出并返回 arr 中最长等差子序列的长度，该子序列中相邻元素之间的差等于 
+//difference 。 
+//
+// 子序列 是指在不改变其余元素顺序的情况下，通过删除一些元素或不删除任何元素而从 arr 派生出来的序列。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：arr = [1,2,3,4], difference = 1
+//输出：4
+//解释：最长的等差子序列是 [1,2,3,4]。 
+//
+// 示例 2： 
+//
+// 
+//输入：arr = [1,3,5,7], difference = 1
+//输出：1
+//解释：最长的等差子序列是任意单个元素。
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：arr = [1,5,7,8,5,3,4,2,1], difference = -2
+//输出：4
+//解释：最长的等差子序列是 [7,5,3,1]。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= arr.length <= 10⁵ 
+// -10⁴ <= arr[i], difference <= 10⁴ 
+// 
+// Related Topics 数组 哈希表 动态规划 👍 120 👎 0
+
+
+package leetcode
+//leetcode submit region begin(Prohibit modification and deletion)
+func longestSubsequence(arr []int, difference int) int {
+	dp := make(map[int]int)
+	ans := 1
+	for i := 0; i < len(arr); i++ {
+		dp[arr[i]] = dp[arr[i]-difference]+1
+		if dp[arr[i]] > ans {
+			ans = dp[arr[i]]
+		}
+	}
+	return ans
+}
+//leetcode submit region end(Prohibit modification and deletion)
