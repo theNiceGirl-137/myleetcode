917c104eeeb240f9df9869d687a9aac282e497f7
theNiceGirl-137 <sqllQAQ@gmail.com>
2021-11-06 11:58:09 +0800

    剑指 Offer 26 --> 树

---
+++ leetcode/editor/cn/[剑指 Offer 26]shu_de_zi_jie_gou_lcof.go
@@ -0,0 +1,62 @@
+//输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构) 
+//
+// B是A的子结构， 即 A中有出现和B相同的结构和节点值。 
+//
+// 例如: 
+//给定的树 A: 
+//
+// 3 
+// / \ 
+// 4 5 
+// / \ 
+// 1 2 
+//给定的树 B： 
+//
+// 4 
+// / 
+// 1 
+//返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。 
+//
+// 示例 1： 
+//
+// 输入：A = [1,2,3], B = [3,1]
+//输出：false
+// 
+//
+// 示例 2： 
+//
+// 输入：A = [3,4,5,1,2], B = [4,1]
+//输出：true 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 10000 
+// Related Topics 树 深度优先搜索 二叉树 👍 386 👎 0
+
+
+package leetcode
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSubStructure(A *TreeNode, B *TreeNode) bool {
+	return A != nil && B != nil && (recur(A, B) || isSubStructure(A.Left, B) || isSubStructure(A.Right, B))
+}
+
+func recur(A, B *TreeNode) bool {
+	// B 越过叶子节点，说明匹配完成
+	if B == nil {
+		return true
+	}
+	// 不匹配
+	if A == nil || A.Val != B.Val {
+		return false
+	}
+	return recur(A.Left, B.Left) && recur(A.Right, B.Right)
+}
+//leetcode submit region end(Prohibit modification and deletion)
