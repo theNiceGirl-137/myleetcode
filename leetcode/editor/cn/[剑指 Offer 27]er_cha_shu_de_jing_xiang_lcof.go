938a3e16aff4a2bd4ccbd91c48a109f413410b23
theNiceGirl-137 <sqllQAQ@gmail.com>
2021-11-10 11:40:19 +0800

    剑指 Offer 27 --> 树

---
+++ leetcode/editor/cn/[剑指 Offer 27]er_cha_shu_de_jing_xiang_lcof.go
@@ -0,0 +1,55 @@
+//请完成一个函数，输入一个二叉树，该函数输出它的镜像。 
+//
+// 例如输入： 
+//
+// 4 
+// / \ 
+// 2 7 
+// / \ / \ 
+//1 3 6 9 
+//镜像输出： 
+//
+// 4 
+// / \ 
+// 7 2 
+// / \ / \ 
+//9 6 3 1 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：root = [4,2,7,1,3,6,9]
+//输出：[4,7,2,9,6,3,1]
+// 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 1000 
+//
+// 注意：本题与主站 226 题相同：https://leetcode-cn.com/problems/invert-binary-tree/ 
+// Related Topics 树 深度优先搜索 广度优先搜索 二叉树 👍 188 👎 0
+
+
+package leetcode
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func mirrorTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	mirrorTree(root.Left)
+	mirrorTree(root.Right)
+	root.Left, root.Right = root.Right, root.Left
+	return root
+}
+//leetcode submit region end(Prohibit modification and deletion)
